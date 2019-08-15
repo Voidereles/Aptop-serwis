48c5e137fa481775b1ada6e7bc1deb0f8673c944
Maciej <maciej.zelazo@protonmail.com>
2019-08-15 23:11:26 +0200

    New sections added

--- script.js
+++ script.js
@@ -23,3 +23,9 @@ if (window.innerWidth > 992) {
     });
 }
 
+$('.nav-link, .btn, header__nav-link, .navbar-brand').click(function () {
+    var sectionTo = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(sectionTo).offset().top
+    }, 1500);
+});
