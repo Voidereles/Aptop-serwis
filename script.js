79b991ce5c22b4e340b7e072d036b3b51d297bee
Maciej <maciej.zelazo@protonmail.com>
2019-08-15 17:32:23 +0200

    background animations

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-        $('#header').addClass('header--scrolled');
-        $('#headerLogo').addClass('header--scrolled__logo');
-        $('#headerNavbar').addClass('header--scrolled__logo');
-        $('#headerNavPills').addClass('header--scrolled__nav-pills')
-    } else {
-        $('#header').removeClass('header--scrolled');
-        $('#headerLogo').removeClass('header--scrolled__logo');
-        $('#headerNavbar').removeClass('header--scrolled__logo');
-        $('#headerNavPills').removeClass('header--scrolled__nav-pills')
-    }
-});
+if (window.innerWidth > 992) {
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() > 100) {
+            $('#header').addClass('header--scrolled');
+            $('#headerLogo').addClass('header--scrolled__logo');
+            $('#headerNavPills').addClass('header--scrolled__nav-pills')
+        }
+
+        else {
+            $('#header').removeClass('header--scrolled');
+            $('#headerLogo').removeClass('header--scrolled__logo');
+            $('#headerNavPills').removeClass('header--scrolled__nav-pills')
+        }
+    });
+
+    const background = document.querySelector("#start");
+
+
+    window.addEventListener("mousemove", (e) => {
+        background.style.backgroundPositionX = (-e.offsetX) / 50 + "px";
+        background.style.backgroundPositionX = (-e.offsetX) / 50 + "px";
+    });
+}
+
